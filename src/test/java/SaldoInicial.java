57b704c06f63074991510006fe59642261ec314e
MariliaA1403 <marilia.albuquerquelima@gmail.com>
2025-09-15 19:19:44 -0300

    Update SaldoInicial.java

--- src/test/java/SaldoInicial.java
+++ src/test/java/SaldoInicial.java
@@ -1,16 +1,25 @@
+package com.example;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Test;
-
-import com.example.DigitalWallet;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class SaldoInicial {
-        void deveConfigurarSaldoInicialCorreto() {
-            
-        }
 
-        void deveLancarExcecaoParaSaldoInicialNegativo() {
-            
-        }
+    @ParameterizedTest
+    @ValueSource(doubles = {10, 0, 20})
+    void saldoInicialDeveSerConfiguradoCorretamente(double saldo) {
+        DigitalWallet digitalWallet = new DigitalWallet("UsuarioTeste", saldo);
+        assertEquals(saldo, digitalWallet.getBalance(), 0.0001);
     }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-10, -5, -20})
+    void saldoInicialNegativoDeveLancarExcecao(double saldo) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new DigitalWallet("UsuarioTeste", saldo);
+        });
+    }
+}
